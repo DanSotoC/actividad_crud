06c3f18c31ac80ebb28a888317c79d6dd0dd4f0e
DanSotoC <danielsotocorrea21@hotmail.com>
2021-05-03 16:30:36 -0400

    PersonaController v2.0

--- actividad_crud/src/main/java/com/everis/formativa/Controllers/PersonaController.java
+++ actividad_crud/src/main/java/com/everis/formativa/Controllers/PersonaController.java
@@ -1,5 +1,55 @@
 package com.everis.formativa.Controllers;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.everis.formativa.Models.Persona;
+import com.everis.formativa.Services.PersonaService;
+
+
+@Controller
 public class PersonaController {
+	@Autowired
+	PersonaService personService;
+	
+	@RequestMapping("/personas")
+	public String personas(@RequestParam(value ="nombre") String nombre,
+			@RequestParam(value ="apellido") String apellido,
+			@RequestParam(value ="rut") String rut,
+			@RequestParam(value ="cargo") String email){
+		Persona person =  new Persona();
+		person.setNombre(nombre);
+		person.setApellido(apellido);
+		person.setRut(rut);
+		person.setEmail(email);
 		
+		person = personService.guardarPersona(person);
+		return "redirect:/";
+	}
+	@RequestMapping("/tabla_personas")
+	public String tablaPersonas(Model modelo){
+		ArrayList<Persona> personList = new ArrayList<Persona>();
+		personList = (ArrayList<Persona>)personService.allPersons();
+		int largo = personList.size();
+		modelo.addAttribute("personList",personList);
+		modelo.addAttribute("largo",largo);
+		return "PersonaList.jsp";
+	}
+	
+	@RequestMapping("/personas/eliminar/{id}")
+	public String eliminarPersona(@PathVariable("id") Long id){
+		personService.deleteById(id);
+		return "redirect:/tabla_personas";
+	}
+	@RequestMapping("/personas/editar/{id}")
+	public String editPersona(@PathVariable("id") Long id){
+		
+		return "";
+	}
 }
--- actividad_crud/src/main/java/com/everis/formativa/Services/PersonaService.java
+++ actividad_crud/src/main/java/com/everis/formativa/Services/PersonaService.java
@@ -1,12 +1,28 @@
 package com.everis.formativa.Services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.everis.formativa.Models.Persona;
 import com.everis.formativa.Repository.PersonaRepository;
 
 public class PersonaService {
 
 	@Autowired
-	PersonaRepository PersonaRepo;
+	PersonaRepository personaRepo;
+
+	public Persona guardarPersona(Persona persona) {
+		return personaRepo.save(persona) ;
+	}
+
+	public List<Persona> allPersons() {
+		return personaRepo.findAll();
+	}
+
+	public void deleteById(Long id) {
+		personaRepo.deleteById(id);
+	}
 
 }
